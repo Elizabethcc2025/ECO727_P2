204318507920b4eb0cf3f09153a23e895fe360f5
Elizabeth <ecc.programacion@gmail.com>
2026-06-01 21:17:23 -0600

    Modificación2

--- backend/TiposCafe/models.py
+++ backend/TiposCafe/models.py
@@ -5,18 +5,18 @@ from django.db import models
 from django.db import models
 
 
-class Cancion(models.Model):
+class TiposCafe(models.Model):
     #agregando 9 campos más el id de tipo int que se genera en automatico seran 10
-    titulo = models.CharField(max_length=200)
-    grupo_interprete = models.CharField(max_length=200)
-    genero = models.CharField(max_length=100)
-    nacionalidad = models.CharField(max_length=100)
-    fecha_lanzamiento = models.DateField()
-    idioma = models.CharField(max_length=100)
-    duracion_minutos = models.FloatField()
-    video = models.URLField(max_length=300)
+    id = models.IntegerField(primary_key=True)  # Identificador único
+    nombre_cliente = models.CharField(max_length=200)
+    correo_electronico = models.EmailField(max_length=200)
+    telefono_contacto = models.CharField(max_length=20)
+    tipo_cafe_preferido = models.CharField(max_length=100)
+    tamano_bebida = models.CharField(max_length=50)
+    nivel_intensidad = models.CharField(max_length=50)
+    tipo_leche = models.CharField(max_length=100)
     comentarios = models.TextField(blank=True, null=True)
-
+    fecha_hora_pedido = models.DateTimeField()
 
     def __str__(self):
         return self.titulo
