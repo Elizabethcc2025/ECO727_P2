8c7f9453b82b496afa19d31d272b54530301f609
Elizabeth <ecc.programacion@gmail.com>
2026-06-03 12:56:39 -0600

    Modificaciónaxios2

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -3,5 +3,5 @@ import axios from "axios";
 
 // Conexión frontend (Netlify) → backend (Railway)
 export default axios.create({
-  baseURL: "eco727p2-production-5dcb.up.railway.app/api/",
+  baseURL: "http://eco727p2-production-5dcb.up.railway.app/api/",
 });
